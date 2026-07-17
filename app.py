30e35069383b558b654b123149fff32f2aa48f2c
riskaayuuu <145608136+riskaayuuu@users.noreply.github.com>
2026-07-17 19:45:29 +0700

    Update

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ app.config['MAIL_USE_TLS']  = True
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_EMAIL")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
 app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_EMAIL")
 
 mongo.init_app(app)
 JWTManager(app)
